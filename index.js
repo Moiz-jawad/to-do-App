f3e763d8e3daddc54647940b22aa37830a060f6a
Moiz Jawad <moizxahito123@gmail.com>
2025-11-05 20:29:43 +0500

    Add CRUD views and style for Todo app
    Introduces new EJS views for adding, updating, and deleting todos, and updates the index view with improved layout and Bootstrap integration. Adds a global CSS stylesheet for consistent styling. Updates Express routes to serve the new views and static assets. Also defines a basic Mongoose schema for todos.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const ejs = require("ejs");
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
+const { type } = require("os");
+
+const app = express();
 
 const PORT = 8000;
 
@@ -14,9 +18,58 @@ mongoose
   .catch((err) => {
     console.log(err.message);
   });
-const app = express();
 
-app.set("view-engine", "ejs");
+const todoSchema = mongoose.Schema(
+  {
+    title: { type: String, require: true },
+    desc: String,
+  },
+  { timeStamp: true }
+);
+
+const todo = mongoose.model("todo", todoSchema);
+
+app.set("view engine", "ejs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  try {
+    res.render("index");
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+
+app.get("/add-todo", (req, res) => {
+  try {
+    res.render("newTodos");
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+app.get("/update-todo", (req, res) => {
+  try {
+    res.render("updateTodos");
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+app.get("/delete-todo", (req, res) => {
+  try {
+    res.render("deleteTodos");
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
