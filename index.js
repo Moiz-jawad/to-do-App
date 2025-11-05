e33238cabc4d758ee1b5fa23f90e93d6c2cc4d24
Moiz Jawad <moizxahito123@gmail.com>
2025-11-05 15:52:45 +0500

    Add MongoDB connection and initial EJS view
    Set up Mongoose connection to local MongoDB and added a basic EJS template for the todo app interface. This prepares the project for further development of todo features.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const express = require("express");
 const ejs = require("ejs");
+const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = 8000;
 
+const connectionUrl = "mongodb://localhost/27017/todoDb";
+
+mongoose
+  .connect(connectionUrl)
+  .then(() => {
+    console.log("database connection established");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 const app = express();
 
 app.set("view-engine", "ejs");
